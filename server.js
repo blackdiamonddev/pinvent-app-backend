255bb6e65ce3843182ad2b26fe0366f270ded963
blackdiamonddev <homeydude@gmail.com>
2023-04-05 18:02:13 -0600

    Added Cors for public access

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://pinvent-app-hazel.vercel.app",
     credentials: true,
   })
 );
